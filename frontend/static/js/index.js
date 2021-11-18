734f9cda7b3937bd05471d69bd1d2729ec9f6c63
Camille <camille.franc.pro@gmail.com>
2021-11-18 10:22:34 +0100

    Feat(routing): retreive route params

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,71 +1,63 @@
-"use strict";
+import Home from "./views/Home.js";
+import PresentationView from "./views/PresentationView.js";
+import TreatmentsView from "./views/TreatmentsView.js";
+import ContactView from "./views/ContactView.js";
+
+const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+
+const getParams = match => {
+    const values = match.result.slice(1);
+    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+
+    return Object.fromEntries(keys.map((key, i) => {
+        return [key, values[i]];
+    }));
+};
+
+const navigateTo = url => {
+    history.pushState(null, null, url);
+    router();
+};
 
 const router = async () => {
-  const routes = [
-    { path: "/" },
-    { path: "/presentation"},
-    { path: "/traitements"},
-    { path: "/contact"}
-  ]
+    const routes = [
+        { path: "/", view: Home },
+        { path: "/presentation", view: PresentationView },
+        { path: "/traitements", view: TreatmentsView },
+        { path: "/contact", view: ContactView }
+    ];
 
-  const potentialMatches = routes.map((route) => {
-    const result = {
-      route,
-      result: location.pathname
-    }
-    return result
-  })
-
-  let match = potentialMatches.find( potentialMatch => potentialMatch.result !== null)
-  /* Route not found - return first route OR a specific "not-found" route */
-  if(!match) {
-    match = {
-      route: routes[0],
-      result: [location.pathname]
-    }
-  }
-}
-
-const navigateTo = (url) => {
-  history.pushState(null, null, url);
-  router()
-}
-
-function animateText() {
-  let delay = 100,
-    delay_start = 0,
-    contents,
-    letters;
-
-  document.querySelectorAll('.animate-text').forEach(function (elem) {
-    contents = elem.textContent.trim()
-
-    elem.textContent = "";
-    letters = contents.split("");
-    elem.style.visibility = 'visible';
-
-    letters.forEach(function (letter, index_1) {
-      setTimeout(function () {
-        var span = document.createElement('span');
-        span.innerHTML = letter.replace(/ /, '&nbsp;');
-        elem.appendChild(span);
-      }, delay_start + delay * index_1)
+    // Test each route for potential match
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            result: location.pathname.match(pathToRegex(route.path))
+        };
     });
-    delay_start += delay * letters.length;
-  })
-}
 
-window.addEventListener('DOMContentLoaded', async (event) => {
-  document.body.addEventListener('click', e => {
-    if (e.target.matches['data-link']) {
-      e.preventDefault();
-      navigateTo(e.target.href)
+    let match = potentialMatches.find(potentialMatch => potentialMatch.result !== null);
+
+    if (!match) {
+        match = {
+            route: routes[0],
+            result: [location.pathname]
+        };
     }
-  });
-  /* Document has loaded -  run the router! */
-  router();
 
-  /* Enables to run the router when the user navigates with the back and forward buttons */
-  document;addEventListener('popstate', router);
-  animateText();
+    const view = new match.route.view(getParams(match));
+
+    document.querySelector("#app").innerHTML = await view.getHtml();
+};
+
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
+
+    router();
 });
