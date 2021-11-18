fd86360df32ee7ad2c9e21f9333229ff7406e534
Camille <camille.franc.pro@gmail.com>
2021-11-18 10:25:06 +0100

    Feat(view): Extract html from index to Home view

---
+++ frontend/static/js/views/Home.js
@@ -0,0 +1,38 @@
+import AbstractView from "./AbstractView.js"
+
+export default class extends AbstractView {
+  constructor(params) {
+    super(params)
+    this.setTitle('Home')
+  }
+
+  async getHtml() {
+    return `
+    <nav>
+      <div class="home__header">
+        <a href="/" class="animate-text">
+          <h1>Dr Bruyère-Franc</h1>
+        </a>
+      </div>
+      <div class="home__footer">
+        <a href="/presentation" class="home__footer__section" data-link>
+          <svg x="580" y="90" viewBox="0 0 56 6.5">
+            <text x="0" y="5">PRESENTATION</text>
+          </svg>
+        </a>
+        <a href="/traitements" class="home__footer__section" data-link>
+          <svg x="560" y="90" viewBox="0 0 56 6.5">
+            <text x="0" y="5">TRAITEMENTS</text>
+          </svg>
+        </a>
+        <a href="/contact" class="home__footer__section" data-link>
+          <svg x="560" y="90" viewBox="0 0 56 6.5">
+            <text x="0" y="5">CONTACT</text>
+          </svg>
+        </a>
+      </div>
+    </nav>
+   
+    `;
+  }
+}
