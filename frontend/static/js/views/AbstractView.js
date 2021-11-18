37fb4f3b238d027db3862d9da049178a82b4b179
Camille <camille.franc.pro@gmail.com>
2021-11-18 12:08:33 +0100

    Fix: Remove console.log()

--- frontend/static/js/views/AbstractView.js
+++ frontend/static/js/views/AbstractView.js
@@ -1,8 +1,6 @@
 export default class AbstractView {
   constructor(params) {
     this.params = params;
-    console.log("passing in abstract view")
-    console.log('params', params)
   }
 
   setTitle(title){
